5b8bb3feabfa430ba620d45428240a9dbce35e8c
Zhang Shuyang <zhangshuyang@outlook.com>
2020-11-16 11:29:28 +0800

    fixed some bug of tab&spaces

--- mabinogi.py
+++ mabinogi.py
@@ -18,17 +18,17 @@ lower_interval_time = interval_time - 1000 if interval_time > 1000 else 0
 higher_interval_time = interval_time + 1000 if interval_time > (0-1000) else 0
 
 if hwnd <= 0:
-	print("Cannot detect the window, closing...")
-	time.sleep(5)
-	exit(0)
+    print("Cannot detect the window, closing...")
+    time.sleep(5)
+    exit(0)
 else:
-	print("Window detected, starting...")
+    print("Window detected, starting...")
 
-	while True:
-		win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, keycode, 0)
-		time.sleep(float(random.randint(100, 400) / 1000))
-		win32api.PostMessage(hwnd, win32con.WM_KEYUP, keycode, 0)
-                time.sleep(float(random.randint(lower_interval_time, higher_interval_time) / 1000))
+    while True:
+        win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, keycode, 0)
+        time.sleep(float(random.randint(100, 400) / 1000))
+        win32api.PostMessage(hwnd, win32con.WM_KEYUP, keycode, 0)
+        time.sleep(float(random.randint(lower_interval_time, higher_interval_time) / 1000))
 
 
 
